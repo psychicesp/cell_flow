c70d220ab9980e0fd89f14accb85c7cf5288a56e
Eric S Penrose <ericspenrose@gmail.com>
2024-02-09 20:40:33 -0600

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -5,7 +5,6 @@ import sys
 # from src.sql.execute import phoenix, _the_technology
 from src.data_extraction.project_parse import read_project
 
-
 file_location = sys.argv[1]
 
 read_project(file_location)
